524103b8afca8b03072029b5bf9cdd88003859cf
JIM <97283576+kifouanijim@users.noreply.github.com>
2025-01-30 21:28:17 +0100

    Résolution du conflit

---
+++ src/Controller/ReviewsController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Reviews;
+use App\Form\ReviewsType;
+use App\Repository\ReviewsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/reviews')]
+final class ReviewsController extends AbstractController{
+    #[Route(name: 'app_reviews_index', methods: ['GET'])]
+    public function index(ReviewsRepository $reviewsRepository): Response
+    {
+        return $this->render('reviews/index.html.twig', [
+            'reviews' => $reviewsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_reviews_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $review = new Reviews();
+        $form = $this->createForm(ReviewsType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($review);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reviews_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('reviews/new.html.twig', [
+            'review' => $review,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_reviews_show', methods: ['GET'])]
+    public function show(Reviews $review): Response
+    {
+        return $this->render('reviews/show.html.twig', [
+            'review' => $review,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_reviews_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Reviews $review, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ReviewsType::class, $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_reviews_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('reviews/edit.html.twig', [
+            'review' => $review,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_reviews_delete', methods: ['POST'])]
+    public function delete(Request $request, Reviews $review, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$review->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($review);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_reviews_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Security/UsersAuthenticator.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Psr\Log\LoggerInterface;
+
+class UsersAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    private UrlGeneratorInterface $urlGenerator;
+    private UserRepository $userRepository;
+    private LoggerInterface $logger;
+
+    public function __construct(
+        UrlGeneratorInterface $urlGenerator,
+        UserRepository $userRepository,
+        LoggerInterface $logger
+    ) {
+        $this->urlGenerator = $urlGenerator;
+        $this->userRepository = $userRepository;
+        $this->logger = $logger;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+        $csrfToken = $request->request->get('_csrf_token');
+
+        // Log des informations de connexion (sans le mot de passe pour la sécurité)
+        $this->logger->info("Tentative de connexion pour l'email : {email}", ['email' => $email]);
+
+        // Sauvegarde du dernier email utilisé
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+
+        return new Passport(
+            new UserBadge($email, function ($userIdentifier) {
+                $user = $this->userRepository->findOneByEmail($userIdentifier);
+                if (!$user) {
+                    $this->logger->warning("Utilisateur non trouvé : {email}", ['email' => $userIdentifier]);
+                }
+                return $user;
+            }),
+            new PasswordCredentials($password),
+            [
+                new CsrfTokenBadge('authenticate', $csrfToken),
+                new RememberMeBadge(),
+            ]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
+            return new RedirectResponse($targetPath);
+        }
+
+        $this->logger->info("Connexion réussie pour : {email}", ['email' => $token->getUser()->getUserIdentifier()]);
+
+        return new RedirectResponse($this->urlGenerator->generate('app_dashboard'));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->urlGenerator->generate(self::LOGIN_ROUTE);
+    }
+}
